758ed7734e8220d28bd20d7503247b8a5aa4f449
Shawn Recinto <shawn.recinto@gmail.com>
2017-07-11 21:00:38 +0000

    added post oidc redirect config

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ okta = {
     "oidc_client_id": "<okta oidc app client id>",
     "oidc_client_secret": "<okta oidc app client secret>",
     "redirect_uri": "<application oidc post handler url>",
-    "app_host": "<application url>"
+    "app_host": "<application url>",
+    "post_oidc_redirect": "<application url to redirect to after oidc token is granted>"
 }
